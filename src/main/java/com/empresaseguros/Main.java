d9d468c8e8eee4b60239964c51183c006385fe21
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2025-07-14 20:59:16 -0300

    commit
    Agregue la opcion de insertar clientes en la base de datos

--- src/main/java/com/empresaseguros/Main.java
+++ src/main/java/com/empresaseguros/Main.java
@@ -1,6 +1,7 @@
 package com.empresaseguros;
 
 
+import com.empresaseguros.data.ClienteDAO;
 import com.empresaseguros.model.Cliente;
 import com.empresaseguros.model.Propiedad;
 import com.empresaseguros.utils.Conexion;
@@ -12,6 +13,17 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
+    Cliente cliente1 = new Cliente("Ramona","Barzola","24261506",LocalDate.parse("1978-10-30"),1195000);
+
+    ClienteDAO clienteConexion = new ClienteDAO();
+
+    boolean exito = clienteConexion.insertarCliente(cliente1);
+
+        if(exito){
+            System.out.println("Insercion exitosa!");
+        } else {
+            System.out.println("Hubo un error.");
+        }
 
     }
 }
--- src/main/java/com/empresaseguros/data/ClienteDAO.java
+++ src/main/java/com/empresaseguros/data/ClienteDAO.java
@@ -1,12 +1,35 @@
 package com.empresaseguros.data;
 
 import com.empresaseguros.model.Cliente;
+import com.empresaseguros.utils.Conexion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ClienteDAO {
 
-public void insertarCliente(Cliente cliente){
+public boolean insertarCliente(Cliente cliente){
 
-    
+    String sql = "INSERT INTO clientes (nombre, apellido, dni, fecha_nacimiento, sueldo) VALUES (?,?,?,?,?)";
+
+    try (Connection conectar = Conexion.getConnection();
+         PreparedStatement pstmt = conectar.prepareStatement(sql)) {
+
+        pstmt.setString(1, cliente.getNombre());
+        pstmt.setString(2, cliente.getApellido());
+        pstmt.setString(3, cliente.getDni());
+        pstmt.setDate(4, java.sql.Date.valueOf(cliente.getFecha_nacimiento()));
+        pstmt.setDouble(5, cliente.getSueldo());
+
+        int filasAfectadas = pstmt.executeUpdate();
+        return filasAfectadas > 0;
+
+    } catch (SQLException e){
+        System.out.println("Hubo un error, detalles: " + e.getMessage());
+        return false;
+    }
+
 
 }
 
